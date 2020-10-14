25016b7cd308a92a1c60861dd044f56644473873
Georgi Alexandrov <georgi.alexandrov@telerik.com>
2020-10-14 13:11:49 +0300

    Add BLUETOOTH permission

--- android/src/main/java/com/lg2/eddystone/EddystoneModule.java
+++ android/src/main/java/com/lg2/eddystone/EddystoneModule.java
@@ -291,6 +291,11 @@ public class EddystoneModule extends ReactContextBaseJavaModule {
     ScanSettings settings = new ScanSettings.Builder().setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY).build();
 
     getCurrentActivity().requestPermissions(
+      new String[]{Manifest.permission.BLUETOOTH},
+      1
+    );
+
+    getCurrentActivity().requestPermissions(
       new String[]{Manifest.permission.BLUETOOTH_ADMIN},
       1
     );
